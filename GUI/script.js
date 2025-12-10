076f5f8e0a6b80baec9509e6d8135adac145ac4f
Parker Williamson <parker.d.williamson@gmail.com>
2025-12-10 17:32:16 -0600

    New TTF Function

--- GUI/script.js
+++ GUI/script.js
@@ -82,8 +82,8 @@ async function updatePoleData(){
     let lastPole2Data = data[data.length-1]; //Last entry is Pole 2
 
     //update water levels
-    let pole1waterlvl = Math.round((lastPole1Data.waterlevel / 6.0) * 100) / 100;
-    let pole2waterlvl = Math.round((lastPole2Data.waterlevel / 6.0) * 100) / 100;
+    let pole1waterlvl = Math.round((lastPole1Data.waterlevel) * 100) / 100;
+    let pole2waterlvl = Math.round((lastPole2Data.waterlevel) * 100) / 100;
     
     //update wate level displays
     document.getElementById("pole1lvl").textContent = "Water Level: " + pole1waterlvl + " in";
@@ -112,8 +112,8 @@ async function updatePoleData(){
         let hour = time.getHours();
         let minute = time.getMinutes();
         let second = time.getSeconds();
-        let pole1Level = data[i].waterlevel / 6.0;
-        let pole2Level = data[i+1].waterlevel / 6.0;
+        let pole1Level = data[i].waterlevel;
+        let pole2Level = data[i+1].waterlevel;
         //console.log("Time: ", [hour, minute, second], " Pole1: ", pole1Level, " Pole2: ", pole2Level);
         chartData.push([[hour, minute, second], pole1Level, pole2Level]);
         
@@ -129,29 +129,7 @@ async function updatePoleData(){
         pole2Last10.push(data[i+1]);
     }
 
-    //calcualte linear regression slope (m) and intercept (b) for each pole
-    function linearRegression(levels) {
-        const n = levels.length;
-        let sumX = 0;
-        let sumY = 0;
-        let sumXY = 0;
-        let sumX2 = 0;
-        let dateinit = new Date(levels[0].created_at);
-        let m = 0;
-        let b = 0;
-        for(let i = 0; i < n; i++){
-            let date = new Date(levels[i].created_at);
-            sumX += (date.getTime() - dateinit.getTime()) / 1000; //Convert to seconds
-            sumY += levels[i].waterlevel / 6.0;
-            sumXY += ((date.getTime() - dateinit.getTime()) / 1000) * (levels[i].waterlevel / 6.0);
-            sumX2 += ((date.getTime() - dateinit.getTime()) / 1000) * ((date.getTime() - dateinit.getTime()) / 1000);
-        }
-        m = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX);
-        b = (sumY - m * sumX) / n;
-        console.log('sumX:', sumX, ' sumY:', sumY, ' sumXY:', sumXY, ' sumX2:', sumX2, ' n:', n);
-        console.log("m:", m, " b:", b);
-        return { m, b, dateinit };
-    }
+
     //console.log("Last 10 Pole 1 Data:", pole1Last10);
     let pole1LR = linearRegression(pole1Last10);
     //console.log("Last 10 Pole 2 Data:", pole2Last10);
@@ -160,15 +138,52 @@ async function updatePoleData(){
     //console.log("Pole 2 LR:", pole2LR);
     
     //predict time to reach critical level (6 inches)
-    let pole1ttf = (CRITLVL_THRESHOLD - pole1LR.b) / pole1LR.m; //time in seconds
-    let pole2ttf = (CRITLVL_THRESHOLD - pole2LR.b) / pole2LR.m; //time in seconds
+    let pole1ttf = ((CRITLVL_THRESHOLD - pole1LR.b) / pole1LR.m) - pole1LR.t; //time in seconds
+    let pole2ttf = ((CRITLVL_THRESHOLD - pole2LR.b) / pole2LR.m) - pole2LR.t; //time in seconds
+    
+    const MAXTTF = 20000;
     //update time to full displays 
-    document.getElementById("pole1ttf").textContent = ((pole1ttf > 0 && isFinite(pole1ttf) && pole1LR.m > 0 && pole1ttf < 20000 && pole1waterlvl < 6) ? "Time to Flood: " + Math.floor(pole1ttf/60) + ":" + Math.round(pole1ttf % 60) + " min" : "");
-    document.getElementById("pole2ttf").textContent = ((pole2ttf > 0 && isFinite(pole2ttf) && pole2LR.m > 0 && pole2ttf < 20000 && pole2waterlvl < 6) ? "Time to Flood: " + Math.floor(pole2ttf/60) + ":" + Math.round(pole2ttf % 60) + " min" : "");
-
+    if(pole1ttf > 3600 && pole1ttf < MAXTTF){
+        document.getElementById("pole1ttf").textContent = ((isFinite(pole1ttf) && pole1LR.m > 0) ? "Time to Flood: " + Math.floor(pole1ttf/3600) + ":" + Math.round((pole1ttf / 3600) % 60) + ":" + Math.round(pole1ttf % 60) + " hours" : "");
+    }else if(pole1ttf > 60){
+        document.getElementById("pole1ttf").textContent = ((isFinite(pole1ttf) && pole1LR.m > 0) ? "Time to Flood: " + Math.floor(pole1ttf/60) + ":" + Math.round(pole1ttf % 60) + " min" : "");
+    }else{
+        document.getElementById("pole1ttf").textContent = ((pole1ttf > 0 && isFinite(pole1ttf) && pole1LR.m > 0) ? "Time to Flood: " + Math.round(pole1ttf % 60) + " sec" : "");
+    }
+    if(pole2ttf > 3600 && pole2ttf < MAXTTF){
+        document.getElementById("pole2ttf").textContent = ((isFinite(pole2ttf) && pole2LR.m > 0) ? "Time to Flood: " + Math.floor(pole2ttf/3600) + ":" + Math.round((pole2ttf / 3600) % 60) + ":" + Math.round(pole2ttf % 60) + " hours" : "");
+    }else if(pole2ttf > 60){
+        document.getElementById("pole2ttf").textContent = ((isFinite(pole2ttf) && pole2LR.m > 0) ? "Time to Flood: " + Math.floor(pole2ttf/60) + ":" + Math.round(pole2ttf % 60) + " min" : "");
+    }else{
+        document.getElementById("pole2ttf").textContent = ((pole2ttf > 0 && isFinite(pole2ttf) && pole2LR.m > 0) ? "Time to Flood: " + Math.round(pole2ttf % 60) + " sec" : "");
+    }
 }
 
-
+//calcualte linear regression slope (m) and intercept (b) for each pole
+function linearRegression(levels) {
+    const n = levels.length;
+    let sumX = 0;
+    let sumY = 0;
+    let sumXY = 0;
+    let sumX2 = 0;
+    let dateinit = new Date(levels[0].created_at);
+    let t = 0;
+    for(let i = 0; i < n; i++){
+        let date = new Date(levels[i].created_at);
+        t = (date.getTime() - dateinit.getTime()) / 1000
+        let lvl = levels[i].waterlevel;
+        //console.log("time: " + date.getTime(), "time_diff: " + t, "lvl: " + lvl);
+        sumX += t;
+        sumY += lvl;
+        sumXY += t * lvl;
+        sumX2 += t * t;
+    }
+    let m = (sumXY - (sumX * sumY / n)) / (sumX2 - (sumX * sumX / n));
+    let b = (sumY - m * sumX) / n;
+    //console.log('sumX:', sumX, ' sumY:', sumY, ' sumXY:', sumXY, ' sumX2:', sumX2, ' n:', n);
+    //console.log("m:", m, " b:", b);
+    return { m, b, t };
+}
 //Image Changing Function
 function changeImage(newImagePath) {
     // Get the image element using its ID
@@ -183,7 +198,7 @@ function UpdateData(){
 }
 //Run update functions to update data every 1 second
 setInterval(updatePoleData, 1000);
-setInterval(UpdateData, 1000); 
+//setInterval(UpdateData, 10000); 
 
 
 //Button behavior
